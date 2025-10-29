b0180de7120f63d0497d8a9d5e21b7b9f6ea1e62
europanite <europanite@gmail.com>
2025-10-29 12:42:47 +0900

    fix/settingbar

--- frontend/app/components/SettingsBar.tsx
+++ frontend/app/components/SettingsBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, useWindowDimensions } from "react-native";
+import { View, useWindowDimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 
